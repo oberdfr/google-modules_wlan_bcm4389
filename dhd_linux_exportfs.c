18b070e6c7ed3721b5038b53add66f9c6389cf8d
Will McVicker <willmcvicker@google.com>
2022-04-14 16:23:23 -0700

    bcmdhd: use default_groups in debug_dpc_bounds kobj_type
    dhd_linux_exportfs.c:2997:3: error: field designator 'default_attrs' does not
    refer to any field in type 'struct kobj_type'
            .default_attrs = debug_dpc_bounds_attrs,
             ^
    1 error generated.
    
    Bug: 229037569
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Change-Id: Ie41be84e81d4b1b9d1083f2cafab976808021dc3

--- dhd_linux_exportfs.c
+++ dhd_linux_exportfs.c
@@ -2936,6 +2936,7 @@ static struct attribute *debug_dpc_bounds_attrs[] = {
 	&dhd_attr_ctrl_cpl_post_bound.attr,
 	NULL
 };
+ATTRIBUTE_GROUPS(debug_dpc_bounds);
 
 #define to_dhd_dpc_bounds(k) container_of(k, struct dhd_info, dhd_dpc_bounds_kobj)
 
@@ -2994,7 +2995,7 @@ static struct sysfs_ops dhd_sysfs_dpc_bounds_ops = {
 
 static struct kobj_type dhd_dpc_bounds_ktype = {
 	.sysfs_ops = &dhd_sysfs_dpc_bounds_ops,
-	.default_attrs = debug_dpc_bounds_attrs,
+	.default_groups = debug_dpc_bounds_groups,
 };
 
 /*
