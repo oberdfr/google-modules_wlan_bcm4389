2ca641cf2cf6a05127c69cbca4c85c2f5f9a4730
Will McVicker <willmcvicker@google.com>
2022-04-14 10:34:57 -0700

    Fix build issues related to the merge
    * Fixes undefined variable references
    * Fixes modified function definitions
    
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Change-Id: Ie53c3978ad8362c3f84b748aa7b462bc33c529bc

--- wl_cfg80211.c
+++ wl_cfg80211.c
@@ -2823,7 +2823,7 @@ exit:
 struct wireless_dev *
 wl_cfg80211_add_if(struct bcm_cfg80211 *cfg,
 	struct net_device *primary_ndev,
-	wl_iftype_t wl_iftype, const char *name, u8 *mac)
+	wl_iftype_t wl_iftype, const char *name, const u8 *mac)
 {
 	struct wireless_dev *wdev = NULL;
 	mutex_lock(&cfg->if_sync);
