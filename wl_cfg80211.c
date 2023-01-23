51fb60d227ef3ea7c1a3c894def68ba30a0035d7
André Draszik <draszik@google.com>
2023-01-23 15:12:30 +0000

    bcmdhd: update for v6.2 kernel (cfg80211_port_authorized())
    Kernel commit [1] has changed cfg80211_port_authorized() and now
    this driver here fails compilation.
    
    Update to follow suit.
    
    [1] https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=0ff57171d6d2
        ("cfg80211: Update Transition Disable policy during port authorization")
    
    Change-Id: I044b92a8aa288ab6dfd8c7d2fed69291a628aef1
    Signed-off-by: André Draszik <draszik@google.com>
    (cherry picked from commit e65440b8bdc5eadb08ee9172fd5efb139b55765c)

--- wl_cfg80211.c
+++ wl_cfg80211.c
@@ -22839,7 +22839,7 @@ wl_cfg80211_sup_event_handler(struct bcm_cfg80211 *cfg, bcm_struct_cfgdev *cfgde
 			WL_DBG(("Authorizing Port with BSSID from DHD profile " MACDBG" \n",
 				MAC2STRDBG(curbssid)));
 		}
-		cfg80211_port_authorized(ndev, (const u8 *)curbssid, GFP_KERNEL);
+		cfg80211_port_authorized(ndev, (const u8 *)curbssid, NULL, 0, GFP_KERNEL);
 #elif (LINUX_VERSION_CODE > KERNEL_VERSION(3, 14, 0)) || defined(WL_VENDOR_EXT_SUPPORT)
 		err = wl_cfgvendor_send_async_event(bcmcfg_to_wiphy(cfg), ndev,
 			BRCM_VENDOR_EVENT_PORT_AUTHORIZED, NULL, 0);
